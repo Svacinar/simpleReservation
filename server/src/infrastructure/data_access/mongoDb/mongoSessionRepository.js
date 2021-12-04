f2479d905aa9b6c54160793608e5713227a76503
Patrik Mackerle <p.mackerle@gmail.com>
2021-12-04 15:26:05 +0100

    Fix dateTime filter for available sessions

--- server/src/infrastructure/data_access/mongoDb/mongoSessionRepository.js
+++ server/src/infrastructure/data_access/mongoDb/mongoSessionRepository.js
@@ -2,9 +2,17 @@ const makeSession = require('./models/session');
 
 module.exports = ({connection}) => {
     const Session = makeSession(connection);
-    const getAvailableSessions = async (dateTime) => {
-        const result = await Session.find({dateTime: dateTime, availability: true}).exec();
-        return result;
+    const getAvailableSessions = async (selectedDate) => {
+        const dateTime = new Date(selectedDate);
+        const nextDay = new Date(selectedDate);
+        nextDay.setDate(nextDay.getDate() + 1);
+        return await Session.find({
+            dateTime: {
+                "$gte": dateTime,
+                "$lt": nextDay,
+            },
+            availability: true,
+        }).exec();
     }
     const insertSession = async (session) => {
         const sessionObject = new Session(session)
